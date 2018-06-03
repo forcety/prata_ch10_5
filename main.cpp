96e3ea82c23ee1bab7610360f884428db85724e9
forcety <forcety@mail.ru>
2018-06-03 15:48:03 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include <iostream>
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    std::cout << "Start program\n";
+    {
+
+        std::cout << "...............\n";
+
+    }
+
+    std::cout << "Done.\n";
+    return a.exec();
+}
